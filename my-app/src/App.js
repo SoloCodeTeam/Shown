93ebe07cef855a11419230d938fcf60992db391c
Abdulbosit <96017917+aabdulbost3@users.noreply.github.com>
2023-11-18 23:26:05 +0500

    navbar component ochdim

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import NavbarComponents from './components/NavbarComponents';
+function App() {
+  return (
+    <div className="container">
+      <>
+      <NavbarComponents />
+      </>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/NavbarComponents.jsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+function NavbarComponents() {
+  return (
+      <div className="Navbar">
+        <h1> Navbar</h1>
+      </div>
+  );
+}
+
+export default NavbarComponents;
