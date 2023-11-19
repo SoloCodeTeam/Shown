8172a3dd638b5e59a2c573ce4c6907fe82ad2d93
Ibrohimov <ibrohimovabdulloh573@gmail.com>
2023-11-19 17:28:39 +0500

    ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { FooterComponent } from './components/FooterComponent';
 import Navbar from './components/NavbarComponents\'/NavbarComponents';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div>
       <>
         <Navbar />
+        <FooterComponent/>
       </>
     </div>
   );
