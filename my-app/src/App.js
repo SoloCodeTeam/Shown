5dbf22bb159df7ed63bb7eb32cb636becfe61b45
Abdulbosit <96017917+aabdulbost3@users.noreply.github.com>
2023-11-20 01:31:36 +0500

    header tayyor responsive bn

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,26 +2,21 @@ import './App.css';
 import { CompanyComponent } from './components/CompanyComponent';
 import { FooterComponent } from './components/FooterComponent';
 import Navbar from './components/NavbarComponents\'/NavbarComponents';
-<<<<<<< HEAD
 import HomeSectionComponents from './components/HomeSectionComponents/HomeSectionComponents';
 import HomeSectionComponentsTwo from './components/HomeSectionComponentsTwo/HomeSectionComponentsTwo';
-=======
+import HomeHeaderComponent from './components/HomeHeaderComponent';
 import { WaitComponent } from './components/WaitComponent';
-
->>>>>>> 160a4e8e2f67dfecc8b96ed515176a2c8826abbd
 function App() {
   return (
     <div>
       <>
         <Navbar />
-<<<<<<< HEAD
+        <HomeHeaderComponent />
         <HomeSectionComponents />
         <HomeSectionComponentsTwo />
-=======
         <WaitComponent/>
         <CompanyComponent/>
         <FooterComponent/>
->>>>>>> 160a4e8e2f67dfecc8b96ed515176a2c8826abbd
       </>
     </div>
   );
