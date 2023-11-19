466f0a00b2ef4d23dd016c0aee4953cf637124a4
Ibrohimov <ibrohimovabdulloh573@gmail.com>
2023-11-19 19:28:24 +0500

    other edits

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { FooterComponent } from './components/FooterComponent';
 import Navbar from './components/NavbarComponents\'/NavbarComponents';
+import { WaitComponent } from './components/WaitComponent';
 
 function App() {
   return (
     <div>
       <>
         <Navbar />
+        <WaitComponent/>
         <FooterComponent/>
       </>
     </div>
