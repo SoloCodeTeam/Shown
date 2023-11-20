9adeb93ea03b2af7fc7b35a8b647e3e7fcbecd4b
Ibrohimov <ibrohimovabdulloh573@gmail.com>
2023-11-20 21:13:25 +0500

    modal created

--- my-app/src/components/ClientPanelSidebar/index.jsx
+++ my-app/src/components/ClientPanelSidebar/index.jsx
@@ -1,9 +1,6 @@
 import "./style.css"
 
 export function ClientPanelSidebar() {
-    const SideClick = (e) => {
-        console.log(e);        
-    }
     return(
         <div className="ClientPanelSidebar">
                 <div className="sidetop">
@@ -12,27 +9,27 @@ export function ClientPanelSidebar() {
                 </div>
                 <div className="sidebottom">
                     <button id="dashboard" onClick={(e) => window.location.pathname = e.target.id}>
-                        <i id="dashboard" class="fa-solid fa-table"></i>
+                        <i id="dashboard" className="fa-solid fa-table"></i>
                         <h3 id="dashboard">Dashboard</h3>
                     </button>
                     <button id="ai" onClick={(e) => window.location.pathname = e.target.id}>
-                        <i id="ai" class="fa-solid fa-brain"></i>
+                        <i id="ai" className="fa-solid fa-brain"></i>
                         <h3 id="ai">Ai_Optimizer</h3>
                     </button>
                     <button id="balance" onClick={(e) => window.location.pathname = e.target.id}>
-                        <i id="balance" class="fa-solid fa-landmark"></i>
+                        <i id="balance" className="fa-solid fa-landmark"></i>
                         <h3 id="balance">Balance</h3>
                     </button>
                     <button id="notifications" onClick={(e) => window.location.pathname = e.target.id}>
-                        <i id="notifications" class="fa-solid fa-bell"></i>
+                        <i id="notifications" className="fa-solid fa-bell"></i>
                         <h3 id="notifications">Notifications</h3>
                     </button>
                     <button id="assets" onClick={(e) => window.location.pathname = e.target.id}>
-                        <i id="assets" class="fa-solid fa-gem"></i>
+                        <i id="assets" className="fa-solid fa-gem"></i>
                         <h3 id="assets">Assets</h3>
                     </button>
                     <button id="settings" onClick={(e) => window.location.pathname = e.target.id}>
-                        <i id="settings" class="fa-solid fa-gear"></i>
+                        <i id="settings" className="fa-solid fa-gear"></i>
                         <h3 id="settings">Settings</h3>
                     </button>
                 </div>
--- my-app/src/pages/ClientPanel/index.jsx
+++ my-app/src/pages/ClientPanel/index.jsx
@@ -1,13 +1,13 @@
+import { ClientPanelModal } from "../../components/ClientPanelModal";
 import { ClientPanelSidebar } from "../../components/ClientPanelSidebar"
 import "./style.css"
 
 export function ClientPanel() {
-    console.log(window.location.pathname);
     return(
         <div className="ClientPanel">
         <ClientPanelSidebar/>
         <div className="container">
-            {window.location.pathname == "/balance" ? <h1>Abdulbosit aka siz uchun balance</h1> : <h1>ok</h1>}
+            {window.location.pathname == "/balance" ? <h1>Abdulbosit aka siz uchun balance :)</h1> : <ClientPanelModal/>}
         </div>
         </div>
     )
