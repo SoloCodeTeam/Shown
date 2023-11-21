84b308d35762fd404747ec9864e11443c32f1cf1
Ibrohimov <ibrohimovabdulloh573@gmail.com>
2023-11-21 21:09:41 +0500

    ok

--- my-app/src/components/ClientPanelSidebar/index.jsx
+++ my-app/src/components/ClientPanelSidebar/index.jsx
@@ -5,32 +5,32 @@ export function ClientPanelSidebar() {
         <div className="ClientPanelSidebar">
                 <div className="sidetop">
                     <span>N</span>
-                    <h2>Nice</h2>
+                    <h1>Nice</h1>
                 </div>
                 <div className="sidebottom">
                     <button id="dashboard" onClick={(e) => window.location.pathname = `/client/${e.target.id}`}>
-                        <i id="dashboard" className="fa-solid fa-table"></i>
-                        <h3 id="dashboard">Dashboard</h3>
+                        <h3 id="dashboard" className="fa-solid fa-table"></h3>
+                        <h2 id="dashboard">Dashboard</h2>
                     </button>
                     <button id="ai" onClick={(e) => window.location.pathname = `/client/${e.target.id}`}>
-                        <i id="ai" className="fa-solid fa-brain"></i>
-                        <h3 id="ai">Ai_Optimizer</h3>
+                        <h3 id="ai" className="fa-solid fa-brain"></h3>
+                        <h2 id="ai">Ai_Optimizer</h2>
                     </button>
                     <button id="balance" onClick={(e) => window.location.pathname = `/client/${e.target.id}`}>
-                        <i id="balance" className="fa-solid fa-landmark"></i>
-                        <h3 id="balance">Balance</h3>
+                        <h3 id="balance" className="fa-solid fa-landmark"></h3>
+                        <h2 id="balance">Balance</h2>
                     </button>
                     <button id="notifications" onClick={(e) => window.location.pathname = `/client/${e.target.id}`}>
-                        <i id="notifications" className="fa-solid fa-bell"></i>
-                        <h3 id="notifications">Notifications</h3>
+                        <h3 id="notifications" className="fa-solid fa-bell"></h3>
+                        <h2 id="notifications">Notifications</h2>
                     </button>
                     <button id="assets" onClick={(e) => window.location.pathname = `/client/${e.target.id}`}>
-                        <i id="assets" className="fa-solid fa-gem"></i>
-                        <h3 id="assets">Assets</h3>
+                        <h3 id="assets" className="fa-solid fa-gem"></h3>
+                        <h2 id="assets">Assets</h2>
                     </button>
                     <button id="settings" onClick={(e) => window.location.pathname = `/client/${e.target.id}`}>
-                        <i id="settings" className="fa-solid fa-gear"></i>
-                        <h3 id="settings">Settings</h3>
+                        <h3 id="settings" className="fa-solid fa-gear"></h3>
+                        <h2 id="settings">Settings</h2>
                     </button>
                 </div>
             </div>
