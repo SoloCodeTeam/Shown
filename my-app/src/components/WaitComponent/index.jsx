9fe34d90bd677b66d646d10f9dd3529259cad3a2
Ibrohimov <ibrohimovabdulloh573@gmail.com>
2023-11-21 23:30:06 +0500

    responsive

--- my-app/src/components/WaitComponent/index.jsx
+++ my-app/src/components/WaitComponent/index.jsx
@@ -3,15 +3,15 @@ import img3 from '../../images/img3.png'
 
 export function WaitComponent() {
     return (
-        <div>
         <div className="Wait">
-            <div>
-                <h1>What are you waiting for?</h1>
-                <p>Think of Shown as your automated marketing department, an AI that optimises ads for you, and around-the-clock support. Start under 5 minutes with our smart advertising platform.</p>
-                <a href="#">Create a account</a>
+            <div className="container">
+                <div className="WaitTexts">
+                    <h1>What are you waiting for?</h1>
+                    <p>Think of Shown as your automated marketing department, an AI that optimises ads for you, and around-the-clock support. Start under 5 minutes with our smart advertising platform.</p>
+                    <a href="#">Create a account</a>
+                </div>
+                <img src={img3} alt="simpleimg" />
             </div>
-            <img src={img3} alt="simpleimg" />
-        </div>
         </div>
     )
 }
